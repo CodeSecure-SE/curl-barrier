23b467236229e48cc051ea6f18411f69903d8022
Mark Hermeling <mhermeling@codesecure.com>
2023-12-08 10:19:18 -0500

    Test fix

--- lib/ftp.c
+++ lib/ftp.c
@@ -286,6 +286,7 @@ static CURLcode AcceptServerConnect(struct Curl_easy *data)
   /* Replace any filter on SECONDARY with one listening on this socket */
   result = Curl_conn_tcp_accepted_set(data, conn, SECONDARYSOCKET, &s);
   if(result)
+    Curl_socket_close(data, conn, s);
     return result;
 
   if(data->set.fsockopt) {
