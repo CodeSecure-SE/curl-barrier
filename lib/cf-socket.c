5bc570dc3d0482b47daea0f0a5ec3459cacc5bca
Mark Hermeling <mark@hermeling.org>
2024-04-08 10:16:39 +0000

    Trigger

--- lib/cf-socket.c
+++ lib/cf-socket.c
@@ -97,6 +97,27 @@ static void set_ipv6_v6only(curl_socket_t sockfd, int on)
 #define set_ipv6_v6only(x,y)
 #endif
 
+// The following is copied from GNU Chess 5.0.8
+char *return_append_str(char *dest, const char *s) {
+/* Append text s to dest, and return new result. */
+        char *new_loc;
+        size_t new_len;
+        /* This doesn't have buffer overflow vulnerabilities, because
+           we always allocate for enough space before appending. */
+        if (!dest) {
+                new_loc = (char *) malloc(strlen(s))+1;
+                strcpy(new_loc, s);
+                return new_loc;
+        }
+        new_len = strlen(dest) + strlen(s) + 1;
+        new_loc = (char *) malloc(new_len);
+        strcpy(new_loc, dest);
+        if (!new_loc) return dest; /* Can't do it, throw away the data */
+        strcat(new_loc, s);
+        return new_loc;
+}
+
+
 static void tcpnodelay(struct Curl_easy *data, curl_socket_t sockfd)
 {
 #if defined(TCP_NODELAY)
