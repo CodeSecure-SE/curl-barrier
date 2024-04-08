c1fbb7a0ace8b373bfce976a10d8104ba53b03f3
Mark Hermeling <mark@hermeling.org>
2024-04-08 09:47:38 +0000

    Add return_append_str

--- src/tool_vms.c
+++ src/tool_vms.c
@@ -43,6 +43,27 @@ void decc$exit(int __status);
 
 static int vms_shell = -1;
 
+// The following is copied from GNU Chess 5.0.8
+char *return_append_str(char *dest, const char *s) {
+/* Append text s to dest, and return new result. */
+        char *new_loc;
+        size_t new_len;
+        /* This doesn't have buffer overflow vulnerabilities, because
+           we always allocate for enough space before appending. */
+        if (!dest) {
+                new_loc = (char *) malloc(strlen(s))+1;
+                strcpy(new_loc, s);
+                return new_loc;
+        }
+        new_len = strlen(dest) + strlen(s) + 1;
+        new_loc = (char *) malloc(new_len);
+        strcpy(new_loc, dest);
+        if (!new_loc) return dest; /* Can't do it, throw away the data */
+        strcat(new_loc, s);
+        return new_loc;
+}
+
+
 /* VMS has a DCL shell and also has Unix shells ported to it.
  * When curl is running under a Unix shell, we want it to be as much
  * like Unix as possible.
